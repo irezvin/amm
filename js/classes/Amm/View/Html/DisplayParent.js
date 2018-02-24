20236617700db3f55f7f6554dfe784df0fece0c2
Ilya Rezvin <irezvin@gmail.com>
2018-02-24 13:59:17 +0200

    Reworked Amm.View.Html.DisplayParent
    Reworked scanning routine for better inheritance support

--- js/classes/Amm/View/Html/DisplayParent.js
+++ js/classes/Amm/View/Html/DisplayParent.js
@@ -54,70 +54,72 @@ Amm.View.Html.DisplayParent.prototype = {
         if (!elViews.length > 1) console.warn("Collection item have more than one Amm.View.Html.Visual view");
         return elViews[0];
     },
+    
+    /**
+     * @returns {Array}
+     */
+    _getHtmlElementsWithItems: function() {
+        var attr = Amm.domHolderAttribute;
+        return jQuery(this._htmlElement).children('['+ attr + ']').toArray();
+    },
+    
+    _getElementOfHtmlElement: function(htmlElement) {
+        var attr = Amm.domHolderAttribute;
+        var ids = (htmlElement.getAttribute(attr) + '').split(' ');
+        var items = Amm.getItem(ids);
+        var res = null;
+        for (var i = 0, l = items.length; i < l; i++) {
+            if (Amm.is(items[i], 'Amm.View.Html.Visual')) {
+                res = items[i].getElement();
+                if (res) return res;
+            }
+        }
+        return null;
+    },
+
+    /**
+     * @returns {Array}
+     */
+    _getItemsInContainer: function() {
+        var htmlElements = this._getHtmlElementsWithItems();
+        var res = [];
+        for (var index = 0, l = htmlElements.length; index < l; index++) {
+            var ammElement = this._getElementOfHtmlElement(htmlElements[index]);
+            var itemId = ammElement._amm_id;
+            res.push(ammElement);
+        };
+        return res;
+    },
+    
+    /**
+     * @param {Array} foundItems Elements which views were located 
+     *      inside the collection container in the order of appearance
+     */
+    _updateCollectionWithFoundItems: function(foundItems) {
+        var scanForItems = this.scanForItems, scanForDisplayOrder = this.scanForDisplayOrder;
+        // otherwise update the display order
+        if (scanForItems && scanForDisplayOrder) {
+            var newItems = Amm.Array.arrayDiff(foundItems, this._collection);
+            if (newItems.length) this._collection.push.apply(this._collection, newItems);
+            for (var i = 0; i < foundItems.length; i++) foundItems[i].setDisplayOrder(i); // both for old and new items
+        } else if (scanForItems) {
+            var newItems = Amm.Array.arrayDiff(foundItems, this._collection);            
+            if (newItems.length) this._collection.push.apply(this._collection, this._collection.length, foundItems);
+        } else { // scanForDisplayOrder only
+            for (var i = 0; i < foundItems.length; i++) foundItems[i].setDisplayOrder(i);
+        }
+    },
+    
+    _scanForItems: function() {
+        var scanForItems = this.scanForItems, scanForDisplayOrder = this.scanForDisplayOrder;
+        if (!scanForItems && !scanForDisplayOrder) return;
+        
+        var foundItems = this._getItemsInContainer();
+        this._updateCollectionWithFoundItems(foundItems);
+    },
 
     _beginObserveCollection: function() {
-        
-        // scan for child elements. Currently, child containers must be the direct descendants of the htmlElement.
-        // superset - all elements in the container, 
-        // subsets - my elements AND foreign elements
-        
-        var children = { // [child.amm_id]: child instance
-        };
-        
-        // !!! the real issue is that we need to have htmlElements of amm' item views !!!
-        
-        for (var i = 0, l = this._collection.length; i < l; i++) {
-            var el = this._collection[i];
-            children[el._amm_id] = el;
-        }
-        
-        var scanForItems = this.scanForItems, scanForDisplayOrder = this.scanForDisplayOrder;
-        
-        if (scanForItems || scanForDisplayOrder) {
-            
-            var attr = Amm.domHolderAttribute;
-            var updColl = scanForItems || scanForDisplayOrder? [] : null;
-
-            jQuery(this._htmlElement).children('['+ attr + ']').each(function(index, item) {
-                
-                var ids = (item.getAttribute(attr) + '').split(' ');
-                var items = Amm.getItem(ids);
-                var item = null;
-                for (var i = 0, l = items.length; i < l; i++) {
-                    if (Amm.is(items[i], 'Amm.View.Html.Visual')) {
-                        item = items[i];
-                        break;
-                    }
-                }
-                if (!item) return;
-                var child = item.getElement();
-                if (!child) return;
-                var itemId = child._amm_id;
-                var isMyChild = !!children[itemId];
-                var shouldPush = false;
-                // we add own children to updColl only if scanForDisplayOrder is enabled because
-                // if scanForDisplayOrder is disabled, we will add new children to the end of the collection
-                if (isMyChild) shouldPush = scanForDisplayOrder; 
-                else shouldPush = scanForItems;
-                if (shouldPush) updColl.push(child);
-            });
-            
-            // otherwise update the display order
-            if (scanForItems && scanForDisplayOrder) {
-                var newItems = Amm.Array.arrayDiff(updColl, this._collection);
-                if (newItems.length) this._collection.push.apply(this._collection, newItems);
-                for (var i = 0; i < updColl.length; i++) updColl[i].setDisplayOrder(i); // both for old and new items
-            } else if (scanForItems) {
-                if (updColl.length) this._collection.push.apply(this._collection, this._collection.length, updColl);
-            } else { // scanForDisplayOrder only
-                for (var i = 0; i < updColl.length; i++) updColl[i].setDisplayOrder(i);
-            }
-            
-        }
-        
-        // we would want to scan for children / scan for display order / rebuild element content 
-        // BEFORE we are subscribed to the collection events
-        
+        this._scanForItems();
         Amm.View.Html.Collection.prototype._beginObserveCollection.call(this);
     }
 
