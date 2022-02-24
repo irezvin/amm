e40b354e4133080a85398cef56a92b79ea4eab54
Ilya Rezvin <ilya@rezvin.com>
2022-02-24 09:00:35 +0200

    Added planning dir

---
+++ planning/Amm Spring 2022.mm
@@ -0,0 +1,105 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1645044247172" ID="ID_136198011" MODIFIED="1645044258110" TEXT="Amm Spring 2022">
+<node COLOR="#66ff00" CREATED="1645045603818" ID="ID_643534485" MODIFIED="1645168875178" POSITION="right" TEXT="MultipleDisplayParent + ViewCloning">
+<node CREATED="1645045611582" ID="ID_32232385" MODIFIED="1645168753930" TEXT="&#x43f;&#x43e;&#x442;&#x43e;&#x43c;&#x443; &#x447;&#x442;&#x43e; &#x434;&#x430;&#x432;&#x43d;&#x43e; &#x432;&#x438;&#x441;&#x438;&#x442; &#x44d;&#x442;&#x430; &#x437;&#x430;&#x434;&#x430;&#x447;&#x430; (&#x441; &#x43f;&#x440;&#x43e;&#x448;&#x43b;&#x43e;&#x439; &#x432;&#x435;&#x441;&#x43d;&#x44b;, &#x430; &#x43e;&#x431;&#x434;&#x443;&#x43c;&#x44b;&#x432;&#x430;&#x442;&#x44c; &#x43d;&#x430;&#x447;&#x430;&#x43b; &#x435;&#x449;&#x435; &#x440;&#x430;&#x43d;&#x44c;&#x448;&#x435;)"/>
+<node CREATED="1645045629363" ID="ID_1716008154" MODIFIED="1645168753933" TEXT="&#x43f;&#x43e;&#x442;&#x43e;&#x43c;&#x443; &#x447;&#x442;&#x43e; &#x445;&#x43e;&#x447;&#x435;&#x442;&#x441;&#x44f; &#x43f;&#x435;&#x440;&#x435;&#x43a;&#x43b;&#x44e;&#x447;&#x438;&#x442;&#x44c;&#x441;&#x44f;"/>
+<node COLOR="#66ff00" CREATED="1645168409304" ID="ID_1700950246" MODIFIED="1645168884448" TEXT="! &#x41d;&#x435; &#x444;&#x430;&#x43a;&#x442;, &#x447;&#x442;&#x43e; &#x438;&#x43c;&#x435;&#x43d;&#x43d;&#x43e; &#x44d;&#x442;&#x43e; &#x43c;&#x435;&#x448;&#x430;&#x435;&#x442; &#x43c;&#x43d;&#x435; &#x441;&#x434;&#x435;&#x43b;&#x430;&#x442;&#x44c; &quot;sticky headers&quot;">
+<node COLOR="#66ff00" CREATED="1645168437093" ID="ID_1421253896" MODIFIED="1645168884447" TEXT="Sticky Headers">
+<node CREATED="1645168442088" ID="ID_1458286925" MODIFIED="1645168753933" TEXT="&#x41c;&#x43e;&#x434;&#x435;&#x43b;&#x44c;: &#x434;&#x443;&#x431;&#x43b;&#x438;&#x440;&#x43e;&#x432;&#x430;&#x442;&#x44c; &#x437;&#x430;&#x433;&#x43e;&#x43b;&#x43e;&#x432;&#x43e;&#x43a; &#x442;&#x430;&#x431;&#x43b;&#x438;&#x446;&#x44b; &#x432; &#x43e;&#x442;&#x434;&#x435;&#x43b;&#x44c;&#x43d;&#x43e;&#x43c; &#x44d;&#x43b;&#x435;&#x43c;&#x435;&#x43d;&#x442;&#x435;">
+<node CREATED="1645168526240" ID="ID_1833334666" MODIFIED="1645168753933" TEXT="&#x441;&#x434;&#x435;&#x43b;&#x430;&#x442;&#x44c; &#x43a;&#x43b;&#x43e;&#x43d; view, &#x43d;&#x43e; &#x43e;&#x442;&#x434;&#x435;&#x43b;&#x44c;&#x43d;&#x44b;&#x439; &#x441;&#x43b;&#x443;&#x447;&#x430;&#x439;"/>
+</node>
+<node CREATED="1645168461034" ID="ID_1036312025" MODIFIED="1645168753933" TEXT="&#x41f;&#x440;&#x43e;&#x430;&#x43d;&#x430;&#x43b;&#x438;&#x437;&#x438;&#x440;&#x43e;&#x432;&#x430;&#x442;&#x44c; HandsOnTable, &#x442;&#x430;&#x43c; &#x44d;&#x442;&#x43e; &#x43f;&#x440;&#x430;&#x432;&#x438;&#x43b;&#x44c;&#x43d;&#x43e; &#x441;&#x434;&#x435;&#x43b;&#x430;&#x43d;&#x43e;; &#x432;&#x43d;&#x438;&#x43c;&#x430;&#x442;&#x435;&#x43b;&#x44c;&#x43d;&#x43e; &#x43f;&#x43e;&#x441;&#x43c;&#x43e;&#x442;&#x440;&#x435;&#x442;&#x44c; &#x43d;&#x430; &#x440;&#x430;&#x437;&#x43c;&#x435;&#x442;&#x43a;&#x443;"/>
+</node>
+</node>
+</node>
+<node CREATED="1645044258771" ID="ID_1778866507" MODIFIED="1645168558460" POSITION="right" TEXT="Table + RecordSet">
+<node COLOR="#66ff00" CREATED="1645044314953" ID="ID_1636721472" MODIFIED="1645168884444" TEXT="ErrorControl">
+<node COLOR="#66ff00" CREATED="1645044321814" ID="ID_1120567943" MODIFIED="1645168884445" TEXT="&#x432;&#x43e;&#x437;&#x43c;&#x43e;&#x436;&#x43d;&#x43e;&#x441;&#x442;&#x44c; &#x421;&#x41a;&#x420;&#x42b;&#x422;&#x418;&#x42f; &#x441;&#x43e;&#x43e;&#x431;&#x449;&#x435;&#x43d;&#x438;&#x439;, &#x43a;&#x43e;&#x442;&#x43e;&#x440;&#x44b;&#x435; &#x423;&#x416;&#x415; &#x412;&#x42b;&#x412;&#x41e;&#x414;&#x42f;&#x422;&#x421;&#x42f; &#x432; &#x434;&#x440;&#x443;&#x433;&#x438;&#x445; control&apos;&#x430;&#x445;!">
+<node COLOR="#66ff00" CREATED="1645168600702" ID="ID_763045022" MODIFIED="1645168884446" TEXT="&#x43f;&#x440;&#x435;&#x434;&#x43b;&#x43e;&#x436;&#x435;&#x43d;&#x438;&#x435;: control &quot;&#x441;&#x43e;&#x43e;&#x431;&#x449;&#x430;&#x435;&#x442;&quot; &#x43a;&#x443;&#x434;&#x430;-&#x442;&#x43e; (&#x43c;&#x43e;&#x434;&#x435;&#x43b;&#x438;?!) &#x43e; &#x442;&#x43e;&#x43c;, &#x447;&#x442;&#x43e; &#x432;&#x44b;&#x432;&#x435;&#x43b; &#x43e;&#x448;&#x438;&#x431;&#x43a;&#x443;">
+<node CREATED="1645168665418" ID="ID_1564313713" MODIFIED="1645168679681" TEXT="&#x43a;&#x43b;&#x44e;&#x447;/&#x43f;&#x443;&#x442;&#x44c; &#x432; errors"/>
+<node CREATED="1645168679930" ID="ID_54816196" MODIFIED="1645168688690" TEXT="&#x441;&#x441;&#x44b;&#x43b;&#x43a;&#x430; &#x43d;&#x430; &#x44d;&#x43b;&#x435;&#x43c;&#x435;&#x43d;&#x442;"/>
+</node>
+<node CREATED="1645168616269" ID="ID_1721422643" MODIFIED="1645168641851" TEXT="modelMeta.afterErrorsShown"/>
+<node CREATED="1645168631100" ID="ID_1139810854" MODIFIED="1645168661889" TEXT="&#x434;&#x438;&#x441;&#x43f;&#x435;&#x442;&#x447;&#x435;&#x440; &#x43e;&#x448;&#x438;&#x431;&#x43e;&#x43a; &#x43f;&#x43e;&#x434;&#x43f;&#x438;&#x441;&#x44b;&#x432;&#x430;&#x435;&#x442;&#x441;&#x44f; &#x43d;&#x430; &#x44d;&#x442;&#x43e; &#x441;&#x43e;&#x43e;&#x431;&#x449;&#x435;&#x43d;&#x438;&#x435;"/>
+</node>
+<node CREATED="1645044505862" ID="ID_1086222923" MODIFIED="1645044513792" TEXT="&#x432;&#x43e;&#x437;&#x43c;&#x43e;&#x436;&#x43d;&#x43e;&#x441;&#x442;&#x44c; &quot;collapse&quot; &#x441;&#x43e;&#x43e;&#x431;&#x449;&#x435;&#x43d;&#x438;&#x439;-&#x434;&#x443;&#x431;&#x43b;&#x435;&#x439;"/>
+<node CREATED="1645044514113" ID="ID_337443401" MODIFIED="1645044527520" TEXT="&#x432;&#x43e;&#x437;&#x43c;&#x43e;&#x436;&#x43d;&#x43e;&#x441;&#x442;&#x44c; &#x441;&#x441;&#x44b;&#x43b;&#x43a;&#x438; &#x432; &#x441;&#x43e;&#x43e;&#x431;&#x449;&#x435;&#x43d;&#x438;&#x44f;&#x445;-&#x434;&#x443;&#x431;&#x43b;&#x44f;&#x445; &#x43d;&#x430; &#x440;&#x435;&#x43b;&#x435;&#x432;&#x430;&#x43d;&#x442;&#x43d;&#x44b;&#x435; control&apos;&#x44b;"/>
+</node>
+<node CREATED="1645045309016" FOLDED="true" ID="ID_245991747" MODIFIED="1645168696464" TEXT="RecordStatusView">
+<node CREATED="1645045341710" ID="ID_752134192" MODIFIED="1645045345529" TEXT="map record status to class"/>
+<node CREATED="1645045346210" ID="ID_1018560370" MODIFIED="1645045351145" TEXT="map record status to html"/>
+<node CREATED="1645045351472" ID="ID_1663509376" MODIFIED="1645045581549" TEXT="link to &quot;selected records&quot; or &quot;selected keys&quot; collection; reflect when record is selected (see &quot;selection feature&quot; below)"/>
+</node>
+<node COLOR="#66ff00" CREATED="1645044346456" ID="ID_453499067" MODIFIED="1645168927076" TEXT="Popup">
+<node CREATED="1645045021336" ID="ID_1670125905" MODIFIED="1645045034423" TEXT="&#x447;&#x442;&#x43e;&#x431;&#x44b; &#x437;&#x430;&#x442;&#x435;&#x43c; &#x43c;&#x43e;&#x436;&#x43d;&#x43e; &#x431;&#x44b;&#x43b;&#x43e; &#x441;&#x43c;&#x435;&#x43d;&#x438;&#x442;&#x44c; &#x440;&#x435;&#x430;&#x43b;&#x438;&#x437;&#x430;&#x446;&#x438;&#x44e; &#x43d;&#x430; popper &#x438; &#x442;.&#x43f;."/>
+<node CREATED="1645045041065" ID="ID_1033648544" MODIFIED="1645045050819" TEXT="&#x43f;&#x43e;&#x43a;&#x430; &#x441;&#x438;&#x43b;&#x44c;&#x43d;&#x43e; &#x43d;&#x435; &#x437;&#x430;&#x43b;&#x443;&#x43f;&#x430;&#x442;&#x44c;&#x441;&#x44f;; z-index &#x438; &#x432;&#x441;&#x451;"/>
+</node>
+<node CREATED="1645045206440" ID="ID_1074907860" MODIFIED="1645045215726" TEXT="Cell annotations (using popups)"/>
+<node CREATED="1645044459605" ID="ID_1401225463" MODIFIED="1645044474469" TEXT="Cell with pop-up for error">
+<node CREATED="1645044663550" ID="ID_462450572" MODIFIED="1645044667703" TEXT="Pop-up on focus"/>
+</node>
+<node CREATED="1645045132096" ID="ID_1992042839" MODIFIED="1645045204367" TEXT="Cell with record status: when record has errors, focusing cell will show pop-up with record error info"/>
+<node CREATED="1645044277908" ID="ID_317486055" MODIFIED="1645045278903" TEXT="Amm.Trait.Table.RecordSet"/>
+<node CREATED="1645044816068" FOLDED="true" ID="ID_687295667" MODIFIED="1645168700487" TEXT="SortColumn">
+<node CREATED="1645044858368" ID="ID_1195646670" MODIFIED="1645044883099" TEXT="&#x43f;&#x43e;&#x43a;&#x430;: click on sort element toggles sort"/>
+</node>
+<node CREATED="1645044303744" FOLDED="true" ID="ID_1113042891" MODIFIED="1645168699881" TEXT="FilterRow">
+<node CREATED="1645044843576" ID="ID_472150224" MODIFIED="1645044854316" TEXT="&#x43f;&#x43e;&#x43a;&#x430; &#x441;&#x434;&#x435;&#x43b;&#x430;&#x435;&#x43c; &#x43f;&#x440;&#x43e;&#x441;&#x442;&#x43e; &#x432;&#x432;&#x43e;&#x434; &#x434;&#x43b;&#x44f; omni-filter"/>
+</node>
+<node CREATED="1645044587306" ID="ID_1410628092" MODIFIED="1645044809952" TEXT="?RecordStatusDisplay"/>
+<node CREATED="1645044917291" ID="ID_561890877" MODIFIED="1645044936586" TEXT="?Recordset: fetch throttling"/>
+<node COLOR="#66ff00" CREATED="1645044374875" ID="ID_1296057662" MODIFIED="1645168918966" TEXT="ActionList">
+<node CREATED="1645168963671" ID="ID_1526836937" MODIFIED="1645169105044" TEXT="Action">
+<node CREATED="1645168970345" ID="ID_1395163752" MODIFIED="1645168972231" TEXT="caption"/>
+<node CREATED="1645168972470" ID="ID_1961075162" MODIFIED="1645168974753" TEXT="description"/>
+<node CREATED="1645168974991" ID="ID_245892415" MODIFIED="1645168992354" TEXT="icon"/>
+<node CREATED="1645168992695" ID="ID_278469122" MODIFIED="1645168994613" TEXT="shortcut"/>
+<node CREATED="1645169013289" ID="ID_1610422250" MODIFIED="1645169018266" TEXT="enabled?"/>
+<node CREATED="1645169032053" ID="ID_60876659" MODIFIED="1645169037108" TEXT="checked?"/>
+<node CREATED="1645169037436" ID="ID_920959948" MODIFIED="1645169040467" TEXT="radioGroup"/>
+<node CREATED="1645169062082" ID="ID_1493237593" MODIFIED="1645169068869" TEXT="groupId"/>
+<node CREATED="1645169054820" ID="ID_818655286" MODIFIED="1645169110410" TEXT="MultiAction extends Action">
+<node CREATED="1645169082349" ID="ID_247143657" MODIFIED="1645169085696" TEXT="actions[]"/>
+<node CREATED="1645169302653" ID="ID_851370475" MODIFIED="1645169304942" TEXT="actionable">
+<node CREATED="1645169310048" ID="ID_1771471313" MODIFIED="1645169326708" TEXT="yes: has action"/>
+<node CREATED="1645169327076" ID="ID_728252444" MODIFIED="1645169340603" TEXT="no: serves only as parent for sub-items"/>
+</node>
+</node>
+</node>
+<node CREATED="1645169362607" ID="ID_621654206" MODIFIED="1645169367929" TEXT="ActionView">
+<node CREATED="1645169465281" ID="ID_1405058054" MODIFIED="1645169468169" TEXT="showIcon?"/>
+<node CREATED="1645169468497" ID="ID_1740803113" MODIFIED="1645169479338" TEXT="showCaption?"/>
+<node CREATED="1645169479616" ID="ID_787499177" MODIFIED="1645169484565" TEXT="showShortcut?"/>
+<node CREATED="1645169486198" ID="ID_782072888" MODIFIED="1645169489639" TEXT="blah-blah-blah"/>
+</node>
+<node CREATED="1645169368700" ID="ID_247416841" MODIFIED="1645169372233" TEXT="MultiActionView">
+<node CREATED="1645169397817" ID="ID_1920071818" MODIFIED="1645169400350" TEXT="DropDown"/>
+<node CREATED="1645169392284" ID="ID_1542103317" MODIFIED="1645169397311" TEXT="ComboButton">
+<node CREATED="1645169439391" ID="ID_1775718045" MODIFIED="1645169444716" TEXT="rememberLastChoice"/>
+</node>
+</node>
+</node>
+<node COLOR="#66ff00" CREATED="1645044412249" ID="ID_947474455" MODIFIED="1645168960227" TEXT="Popup + ActionList = Pop-up menu">
+<node COLOR="#ff0000" CREATED="1645169152530" ID="ID_599250690" MODIFIED="1645169194045" TEXT="TODO: &#x43d;&#x430;&#x443;&#x447;&#x438;&#x442;&#x44c;&#x441;&#x44f; flow layout">
+<node CREATED="1645169169952" ID="ID_1079522527" MODIFIED="1645169176753" TEXT="&#x448;&#x430;&#x43f;&#x43a;&#x430;, 100% &#x441;&#x435;&#x440;&#x435;&#x434;&#x438;&#x43d;&#x44b;, &#x43f;&#x43e;&#x434;&#x432;&#x430;&#x43b;"/>
+</node>
+</node>
+<node CREATED="1645044383397" ID="ID_91030052" MODIFIED="1645044944432" TEXT="Dialog">
+<node CREATED="1645045055802" ID="ID_297865255" MODIFIED="1645045093762" TEXT="global depth stack, feature to move dialogs up/down the stack"/>
+<node CREATED="1645044946061" ID="ID_908075839" MODIFIED="1645044954855" TEXT="show/hide columns?"/>
+<node CREATED="1645044955197" ID="ID_1946118716" MODIFIED="1645044957336" TEXT="filter?"/>
+</node>
+<node CREATED="1645044682009" ID="ID_776327095" MODIFIED="1645045248714" TEXT="Selection">
+<node CREATED="1645044685636" ID="ID_199981560" MODIFIED="1645044728101" TEXT="Selection feature (&quot;Ctrl&quot;, &quot;Shift&quot;, &quot;Ctrl-Shift&quot; modes)"/>
+<node CREATED="1645044974373" ID="ID_1087254322" MODIFIED="1645044979334" TEXT="Table: drag-to-select"/>
+<node CREATED="1645044994834" ID="ID_755168687" MODIFIED="1645045004844" TEXT="Feature to &quot;copy&quot; selected cells (no pasting?)"/>
+</node>
+<node CREATED="1645045431885" ID="ID_1223687665" MODIFIED="1645045566626" TEXT="?Recordset: SelectionFeature">
+<node CREATED="1645045453618" ID="ID_234360709" MODIFIED="1645045464932" TEXT="Able to remember keys, not records"/>
+<node CREATED="1645045471186" ID="ID_31984500" MODIFIED="1645045493564" TEXT="Default mode: none + remember selected; all + remember de-selected"/>
+<node CREATED="1645045513124" ID="ID_735880059" MODIFIED="1645045536185" TEXT="Should survive pagination and/or change of filters"/>
+</node>
+</node>
+</node>
+</map>
